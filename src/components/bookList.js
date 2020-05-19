80950d1b63d521ce3e09ece709d7d0c8ab0f8d60
Giridhar Gayake <35455992+gayakegiridhar@users.noreply.github.com>
2020-05-19 14:43:42 +0530

    Hooks Context
    Change in Booklist

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,28 +1,45 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-class booklist extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {context => {
-          const { isLightTheme, dark, light } = context;
-          const Theme = isLightTheme ? light : dark;
-          return (
-            <div
-              className="book-list"
-              style={{ color: Theme.syntax, background: Theme.bg }}
-            >
-              <ul>
-                <li style={{ background: Theme.ui }}>The way of Kings</li>
-                <li style={{ background: Theme.ui }}>The alchemist</li>
-                <li style={{ background: Theme.ui }}>Immortals of meluva</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+// class booklist extends Component {
+//   render() {
+//     return (
+//       <ThemeContext.Consumer>
+//         {context => {
+//           const { isLightTheme, dark, light } = context;
+//           const Theme = isLightTheme ? light : dark;
+//           return (
+//             <div
+//               className="book-list"
+//               style={{ color: Theme.syntax, background: Theme.bg }}
+//             >
+//               <ul>
+//                 <li style={{ background: Theme.ui }}>The way of Kings</li>
+//                 <li style={{ background: Theme.ui }}>The alchemist</li>
+//                 <li style={{ background: Theme.ui }}>Immortals of meluva</li>
+//               </ul>
+//             </div>
+//           );
+//         }}
+//       </ThemeContext.Consumer>
+//     );
+//   }
+// }
+const booklist = () => {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const Theme = isLightTheme ? light : dark;
+
+  return (
+    <div
+      className="book-list"
+      style={{ color: Theme.syntax, background: Theme.bg }}
+    >
+      <ul>
+        <li style={{ background: Theme.ui }}>The way of Kings</li>
+        <li style={{ background: Theme.ui }}>The alchemist</li>
+        <li style={{ background: Theme.ui }}>Immortals of meluva</li>
+      </ul>
+    </div>
+  );
+};
 export default booklist;
